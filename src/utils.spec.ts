27befbc7b74c6c3c18e4e8c7aec3a460369a75a9
mtti <mtti@users.noreply.github.com>
2019-08-12 13:44:50 +0300

    Add suffixUrlFilename()

---
+++ src/utils.spec.ts
@@ -0,0 +1,38 @@
+import { suffixUrlFilename } from './utils';
+
+describe('utils', () => {
+  describe('suffixUrlFilename()', () => {
+    let result: string = '';
+    let error: any = null;
+
+    beforeEach(() => {
+      result = '';
+      error = null;
+    });
+
+    it('works when given bare URL', () => {
+      result = suffixUrlFilename('http://example.com', '-suffixed');
+      expect(result).toBe('http://example.com/-suffixed');
+    });
+
+    it('works with file extension', () => {
+      result = suffixUrlFilename('http://example.com/some-schema.json', '-suffixed');
+      expect(result).toBe('http://example.com/some-schema-suffixed.json');
+    });
+
+    it('works with multiple extensions', () => {
+      result = suffixUrlFilename('http://example.com/some.schema.json', '-suffixed');
+      expect(result).toBe('http://example.com/some-suffixed.schema.json');
+    });
+
+    it('throws when given a non-url', () => {
+      try {
+        result = suffixUrlFilename('not a URL', '-suffixed');
+      } catch (err) {
+        error = err;
+      }
+      expect(result).toBe('');
+      expect(error).toBeTruthy();
+    });
+  });
+});
